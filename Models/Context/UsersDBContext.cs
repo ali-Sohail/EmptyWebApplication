d9430c436d6ce422567145e97983fd8570a5e085
Sohail <soali@q3tech.com>
2020-03-25 17:27:04 +0530

    Add project files.

---
+++ Models/Context/UsersDBContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Models;
+
+namespace Models.Context
+{
+    public partial class UsersDBContext : DbContext
+    {
+        public UsersDBContext()
+        {
+        }
+
+        public UsersDBContext(DbContextOptions<UsersDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                // optionsBuilder.UseSqlServer("Server=.;Database=Demo;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Models
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Address { get; set; }
+    }
+}
