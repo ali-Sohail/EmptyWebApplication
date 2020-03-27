fb4526fc41f8a0263f4d135966b0e7b36b221e1d
Sohail <soali@q3tech.com>
2020-03-27 10:54:17 +0530

    Controller modifyed

--- EmptyWebApplication/Controllers/DaliyLogsController.cs
+++ EmptyWebApplication/Controllers/DaliyLogsController.cs
@@ -25,7 +25,8 @@ namespace EmptyWebApplication.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<DaliyLog>>> GetDaliyLogs()
         {
-            return await _context.DaliyLog.ToListAsync();
+            var data = await _context.DaliyLog.ToListAsync();
+            return data;
         }
 
         // GET: api/DaliyLogs/5
@@ -41,6 +42,44 @@ namespace EmptyWebApplication.Controllers
 
             return daliyLog;
         }
+
+
+
+        [HttpPut]
+        public async Task<IActionResult> PutDaliyLog(DaliyLog daliyLog)
+        {
+            //if (id != daliyLog.Id)
+            //{
+            //    return BadRequest();
+            //}
+            if (daliyLog.Id != null && DaliyLogExists(daliyLog.Id))
+            {
+                _context.Entry(daliyLog).State = EntityState.Modified;
+            }
+            else
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DaliyLogExists(daliyLog.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
 
         // PUT: api/DaliyLogs/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
@@ -80,10 +119,18 @@ namespace EmptyWebApplication.Controllers
         [HttpPost]
         public async Task<ActionResult<DaliyLog>> PostDaliyLog(DaliyLog daliyLog)
         {
-            _context.DaliyLog.Add(daliyLog);
-            await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetDaliyLog", new { id = daliyLog.Id }, daliyLog);
+            if (_context.User.Any(e => e.Id == daliyLog.UserId))
+            {
+                _context.DaliyLog.Add(daliyLog);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction("GetDaliyLog", new { id = daliyLog.Id }, daliyLog);
+            }
+            else
+            {
+                return NotFound();
+            }
         }
 
         // DELETE: api/DaliyLogs/5
@@ -102,9 +149,16 @@ namespace EmptyWebApplication.Controllers
             return daliyLog;
         }
 
-        private bool DaliyLogExists(int id)
+        private bool DaliyLogExists(int? id)
         {
-            return _context.DaliyLog.Any(e => e.Id == id);
+            if (id != null)
+            {
+                return _context.DaliyLog.Any(e => e.Id == id);
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
--- Models/DaliyLog.cs
+++ Models/DaliyLog.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Models
 { 
     public partial class DaliyLog
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public DateTime InDateTime { get; set; }
         public DateTime? OutDateTime { get; set; }
+        [Required]
         public DateTime LogDate { get; set; }
-
+        [Required]
         public int UserId { get; set; }
     }
 }
